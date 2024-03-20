7b0fbd823ac11d8269716bf72028b9add4dc2e0c
sandeshkm25@gmail.com <sandeshkm25@gmail.com>
2024-03-20 16:48:57 +0530

    new project

---
+++ src/main/java/com/mycompany/hurdles_and_paths/Hurdles_and_paths.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hurdles_and_paths;
+
+/**
+ *
+ * @author Dell
+ */
+public class Hurdles_and_paths {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
